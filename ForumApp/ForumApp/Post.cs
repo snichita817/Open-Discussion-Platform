807a94fd0502c95d92fc86a6e7976ff7880fccfb
AlexTindeche <alexandru.tindeche@gmail.com>
2022-11-30 19:20:04 +0200

    Added Subforum and Post

---
+++ ForumApp/ForumApp/Post.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ForumApp
+{
+    public class Post
+    {
+        [Key]
+        public int PostId { get; set; }
+
+        [Required(ErrorMessage = "Titlul este obligatoriu!")]
+        [MinLength(5, ErrorMessage = "Lungimea minima trebuie sa fie de 5 caractere")]
+        [StringLength(100, ErrorMessage = "Lungimea maxima trebuie sa fie de 100 de caractere")]
+        public string PostTitle { get; set; }
+
+        [Required(ErrorMessage = "Continutul este obligatoriu!")]
+        [MinLength(1, ErrorMessage = "Lungimea minima trebuie sa fie de 1 caracter")]
+        [StringLength(10000, ErrorMessage = "Lungimea maxima trebuie sa fie de 10000 de caractere")]
+        public string PostContent { get; set; }
+
+        [Required(ErrorMessage = "Id-ul subforumului trebuie sa fie specificat!")]
+        public int SubforumId { get; set; }
+
+        [Required(ErrorMessage = "Data postarii trebuie sa fie specificata!")]
+        public DateTime PostDate { get; set; }
+
+        [Required(ErrorMessage = "Userul care a creat postarea trebuie sa fie specificat!")]
+        public string Sender { get; set; }
+        
+        
+
+
+    }
+}
