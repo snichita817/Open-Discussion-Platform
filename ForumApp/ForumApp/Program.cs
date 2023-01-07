fb06ca6fdff24207d281cc3470377e31781a1b1a
AlexTindeche <alexandru.tindeche@gmail.com>
2023-01-07 18:33:39 +0200

    Users Button - visible only for admins

--- ForumApp/ForumApp/Program.cs
+++ ForumApp/ForumApp/Program.cs
@@ -50,7 +50,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Sections}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 app.Run();
